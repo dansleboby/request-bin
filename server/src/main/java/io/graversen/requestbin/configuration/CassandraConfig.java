62df5beded13abf3bd00676b0b6059a35524e0bd
Martin Graversen <martin@graversen.io>
2019-09-07 15:32:53 +0200

    Cassandra auto-creates keyspace if not present

--- server/src/main/java/io/graversen/requestbin/configuration/CassandraConfig.java
+++ server/src/main/java/io/graversen/requestbin/configuration/CassandraConfig.java
@@ -1,10 +1,27 @@
 package io.graversen.requestbin.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories;
+
+import java.util.List;
 
 @Configuration
 @EnableReactiveCassandraRepositories(basePackages = {"io.graversen.requestbin.data.cassandra"})
-public class CassandraConfig {
+public class CassandraConfig extends AbstractReactiveCassandraConfiguration {
+    @Override
+    protected String getKeyspaceName() {
+        return "request_bin";
+    }
 
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        final CreateKeyspaceSpecification specification =
+                CreateKeyspaceSpecification.createKeyspace(getKeyspaceName())
+                        .ifNotExists()
+                        .withSimpleReplication();
+
+        return List.of(specification);
+    }
 }
