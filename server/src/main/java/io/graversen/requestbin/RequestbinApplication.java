32b0b299e8c84262e72ba24dcc8c00a77381084e
Martin Graversen <martin@graversen.io>
2019-09-07 14:10:45 +0200

    :tada: Server bootstrap

---
+++ server/src/main/java/io/graversen/requestbin/RequestbinApplication.java
@@ -0,0 +1,13 @@
+package io.graversen.requestbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestbinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestbinApplication.class, args);
+    }
+
+}
