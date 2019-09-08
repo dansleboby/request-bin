64f54035c30553077e3dfa41dff0ebd8ea99533e
Martin Graversen <martin@graversen.io>
2019-09-08 12:34:01 +0200

    Debug static HTML page

--- server/src/main/java/io/graversen/requestbin/configuration/ApiConfig.java
+++ server/src/main/java/io/graversen/requestbin/configuration/ApiConfig.java
@@ -1,9 +1,24 @@
 package io.graversen.requestbin.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
 
 @Configuration
 @ComponentScan(basePackages = "io.graversen.requestbin.api")
 public class ApiConfig {
+    @Bean
+    public RouterFunction<ServerResponse> debugRouter(@Value("classpath:/public/debug.html") Resource html) {
+        return route(GET("/debug"), request -> ok().contentType(MediaType.TEXT_HTML).syncBody(html));
+    }
 }
