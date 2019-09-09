f6ca17312c869b467c7fc1c5a76fe0c7e9067f99
Martin Graversen <martin@graversen.io>
2019-09-09 17:17:22 +0200

    Save, map, emit pipeline

--- server/src/main/java/io/graversen/requestbin/service/RequestBinService.java
+++ server/src/main/java/io/graversen/requestbin/service/RequestBinService.java
@@ -2,10 +2,13 @@ package io.graversen.requestbin.service;
 
 import io.graversen.requestbin.data.cassandra.IRequestByRequestBinRepository;
 import io.graversen.requestbin.data.cassandra.RequestByRequestBinEntity;
+import io.graversen.requestbin.data.dto.Request;
 import io.graversen.requestbin.data.mysql.IRequestBinRepository;
 import io.graversen.requestbin.data.mysql.RequestBinEntity;
 import io.graversen.requestbin.data.service.CreateRequest;
 import io.graversen.requestbin.data.service.CreateRequestBin;
+import io.graversen.requestbin.streaming.Clients;
+import io.graversen.requestbin.streaming.RequestEvent;
 import io.graversen.requestbin.util.Utils;
 import io.graversen.trunk.io.serialization.interfaces.ISerializer;
 import io.graversen.trunk.io.serialization.json.GsonSerializer;
@@ -14,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Service
 @Transactional
@@ -22,6 +27,7 @@ public class RequestBinService {
     private final ISerializer SERIALIZER = new GsonSerializer();
     private final IRequestBinRepository requestBinRepository;
     private final IRequestByRequestBinRepository requestByRequestBinRepository;
+    private final Clients clients;
 
     public RequestBinEntity createNew(CreateRequestBin createRequestBin) {
         final var binId = Utils.binId();
@@ -41,20 +47,44 @@ public class RequestBinService {
         final var queryParameters = SERIALIZER.serialize(createRequest.getQueryParameters());
         final var duration = createRequest.getRequestDuration().toString();
 
-        requestByRequestBinRepository.save(
-                new RequestByRequestBinEntity(
-                        createRequest.getBinId(),
-                        createRequest.getRequestBody(),
-                        queryParameters,
-                        httpHeaders,
-                        createRequest.getIpAddress(),
-                        createRequest.getHttpVerb(),
-                        duration
-                )
+        final var requestEntity = new RequestByRequestBinEntity(
+                createRequest.getBinId(),
+                createRequest.getRequestBody(),
+                queryParameters,
+                httpHeaders,
+                createRequest.getIpAddress(),
+                createRequest.getHttpVerb(),
+                duration
         );
+
+        requestByRequestBinRepository.save(requestEntity)
+                .map(requestDtoMapper())
+                .map(RequestEvent::data)
+                .subscribe(emitRequestEvent(createRequest.getBinId()));
+    }
+
+    public boolean requestBinExists(String binId) {
+        return requestBinRepository.existsByBinIdAndOpenTrue(binId);
     }
 
     public Optional<RequestBinEntity> getByBinId(String binId) {
         return requestBinRepository.findByBinId(binId);
+    }
+
+    private Function<RequestByRequestBinEntity, Request> requestDtoMapper() {
+        return entity -> new Request(
+                entity.getBinId(),
+                entity.getCreatedAt(),
+                entity.getRequestBody(),
+                entity.getQueryParameters(),
+                entity.getHttpHeaders(),
+                entity.getIpAddress(),
+                entity.getHttpVerb(),
+                entity.getRequestDuration()
+        );
+    }
+
+    private Consumer<RequestEvent> emitRequestEvent(String binId) {
+        return requestEvent -> clients.emit(binId, requestEvent);
     }
 }
