9620fedab478108f14ac9a259459bdb8482aae4f
Martin Graversen <martin@graversen.io>
2019-09-21 20:36:04 +0200

    :sparkles: Render HTTP headers

--- client/src/components/http/HttpHeaders/HttpHeaders.js
+++ client/src/components/http/HttpHeaders/HttpHeaders.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
-const HttpHeaders = (props) => (
-    <div className="card">
-        <header className="card-header has-background-light">
-            <p className="card-header-title">
-                HTTP Headers
-            </p>
-        </header>
-        <div className="card-content">
-            <div className="content">
-                <code>// TODO</code>
+const toHeaderRow = (header, value) => <tr><td><strong>{header}</strong></td><td>{value}</td></tr>;
+
+const HttpHeaders = (props) => {
+    const httpHeaders = props.httpHeaders;
+
+    let httpHeadersElement = null;
+
+    if (httpHeaders) {
+        httpHeadersElement = httpHeaders.map(header => toHeaderRow(header.key, header.value));
+    }
+
+    return (
+        <div className="card">
+            <header className="card-header has-background-light">
+                <p className="card-header-title">
+                    HTTP Headers
+                </p>
+            </header>
+            <div className="card-content">
+                <div className="content">
+                    <table className="table is-bordered">
+                        {httpHeadersElement}
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default HttpHeaders;
