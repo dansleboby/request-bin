edfac5358344ee5377de6718afd4a783bc53cc85
Martin Graversen <martin@graversen.io>
2019-09-28 14:52:34 +0200

    :sparkles: Close bins after expiry

--- server/src/main/java/io/graversen/requestbin/configuration/ApiConfigProd.java
+++ server/src/main/java/io/graversen/requestbin/configuration/ApiConfigProd.java
@@ -1,13 +1,33 @@
 package io.graversen.requestbin.configuration;
 
+import io.graversen.requestbin.data.mysql.IRequestBinRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Configuration
 @Profile("prod")
+@EnableScheduling
 @ComponentScan(basePackages = "io.graversen.requestbin.api")
+@RequiredArgsConstructor
 public class ApiConfigProd implements WebFluxConfigurer {
+    private static final Duration BIN_EXPIRY = Duration.ofHours(24);
+    private final IRequestBinRepository requestBinRepository;
 
+    @Scheduled(fixedDelayString = "PT1H")
+    public void cleanUpRequestBins() {
+        final var now = LocalDateTime.now();
+        final var deleteAt = now.minus(BIN_EXPIRY);
+
+        requestBinRepository.findByOpenTrue().stream()
+                .filter(requestBinEntity -> requestBinEntity.getCreatedAt().isBefore(deleteAt))
+                .forEach(requestBinEntity -> requestBinEntity.setOpen(false));
+    }
 }
--- server/src/main/java/io/graversen/requestbin/configuration/AppConfig.java
+++ server/src/main/java/io/graversen/requestbin/configuration/AppConfig.java
@@ -1,40 +1,12 @@
 package io.graversen.requestbin.configuration;
 
-import io.graversen.requestbin.data.cassandra.IRequestByRequestBinRepository;
-import io.graversen.requestbin.data.cassandra.RequestByRequestBinEntity;
-import io.graversen.requestbin.data.dto.Request;
-import io.graversen.requestbin.data.mysql.IRequestBinRepository;
-import io.graversen.requestbin.data.mysql.RequestBinEntity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-import reactor.core.publisher.DirectProcessor;
-import reactor.core.publisher.FluxProcessor;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
 
 @Configuration
 @RequiredArgsConstructor
 @ComponentScan(basePackages = {"io.graversen.requestbin.service", "io.graversen.requestbin.streaming"})
 public class AppConfig {
-    private final IRequestBinRepository requestBinRepository;
-    private final IRequestByRequestBinRepository requestByRequestBinRepository;
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void onApplicationReady() {
-        requestBinRepository.deleteAll();
-        requestBinRepository.save(
-                new RequestBinEntity("test", LocalDateTime.now().plus(Duration.ofDays(1)), "test")
-        );
-
-        requestByRequestBinRepository.deleteAll().block();
-        requestByRequestBinRepository.save(
-                new RequestByRequestBinEntity("test", "body", "query params", "http headers", "ip address", "GET", "dunno lol")
-        ).block();
-        requestByRequestBinRepository.findAll().collectList().block();
-    }
 }
