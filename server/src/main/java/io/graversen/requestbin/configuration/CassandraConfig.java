8a588ce86084e29b1ba18655f2174a0acb0212a3
Martin Graversen <martin@graversen.io>
2019-09-28 15:56:44 +0200

    :whale: Add Cassandra contact point to app properties for Docker Compose

--- server/src/main/java/io/graversen/requestbin/configuration/CassandraConfig.java
+++ server/src/main/java/io/graversen/requestbin/configuration/CassandraConfig.java
@@ -1,5 +1,6 @@
 package io.graversen.requestbin.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
@@ -11,6 +12,12 @@ import java.util.List;
 @Configuration
 @EnableReactiveCassandraRepositories(basePackages = {"io.graversen.requestbin.data.cassandra"})
 public class CassandraConfig extends AbstractReactiveCassandraConfiguration {
+    private final String contactPoints;
+
+    public CassandraConfig(@Value("${spring.data.cassandra.contact-points}") String contactPoints) {
+        this.contactPoints = contactPoints;
+    }
+
     @Override
     protected String getKeyspaceName() {
         return "request_bin";
@@ -34,5 +41,10 @@ public class CassandraConfig extends AbstractReactiveCassandraConfiguration {
     @Override
     public String[] getEntityBasePackages() {
         return new String[]{"io.graversen.requestbin.data.cassandra"};
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
     }
 }
