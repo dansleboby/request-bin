9be8a1e1e55cb2f1234ee62b161ab518ab30fe86
Martin Graversen <martin@graversen.io>
2020-09-06 19:28:25 +0200

    :wrench: Add logging of environment setup

--- server/src/main/java/io/graversen/requestbin/configuration/AppConfig.java
+++ server/src/main/java/io/graversen/requestbin/configuration/AppConfig.java
@@ -2,6 +2,8 @@ package io.graversen.requestbin.configuration;
 
 import io.graversen.requestbin.service.requestbin.CreateRequestBin;
 import io.graversen.requestbin.service.requestbin.RequestBinService;
+import io.graversen.trunk.io.serialization.interfaces.ISerializer;
+import io.graversen.trunk.io.serialization.json.GsonSerializer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -18,11 +20,14 @@ import java.util.function.Consumer;
 @EnableConfigurationProperties(RequestBinProperties.class)
 @ComponentScan(basePackages = {"io.graversen.requestbin.service", "io.graversen.requestbin.streaming"})
 public class AppConfig {
+    private static final ISerializer SERIALIZER = new GsonSerializer();
+
     private final RequestBinProperties requestBinProperties;
     private final RequestBinService requestBinService;
 
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationReady() {
+        log.info("Configuration: {}", SERIALIZER.serialize(requestBinProperties));
         requestBinProperties.getPersistentBins().forEach(createPersistentBinIfNotExists());
     }
 
