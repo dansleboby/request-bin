f2f1784e3ee6dfdbc03cff8ab0a7213bffbe676f
Martin Graversen <martin@graversen.io>
2020-09-06 16:49:41 +0200

    :zap: Spring Boot 2.3.0, add additional configuration properties

--- server/src/main/java/io/graversen/requestbin/configuration/AppConfig.java
+++ server/src/main/java/io/graversen/requestbin/configuration/AppConfig.java
@@ -1,11 +1,13 @@
 package io.graversen.requestbin.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(RequestBinProperties.class)
 @ComponentScan(basePackages = {"io.graversen.requestbin.service", "io.graversen.requestbin.streaming"})
 public class AppConfig {
 
---
+++ server/src/main/java/io/graversen/requestbin/configuration/RequestBinProperties.java
@@ -0,0 +1,18 @@
+package io.graversen.requestbin.configuration;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.util.List;
+
+@Getter
+@ConstructorBinding
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "request-bin")
+public class RequestBinProperties {
+    private final int maxFetchSize;
+    private final String adminSecret;
+    private final List<String> persistentBins;
+}
