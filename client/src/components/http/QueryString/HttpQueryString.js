86fa12a2062522be61c4b30a938b492085103df5
Martin Graversen <martin@graversen.io>
2020-09-07 20:55:35 +0200

    :zap: Don't display query parameters if there are none

--- client/src/components/http/QueryString/HttpQueryString.js
+++ client/src/components/http/QueryString/HttpQueryString.js
@@ -5,31 +5,30 @@ const toQueryParameterRow = (header, value) => <tr key={header}><td><strong>{hea
 const HttpQueryString = (props) => {
     const queryParameters = props.queryParameters;
 
-    let queryParametersElement = null;
+    if (queryParameters && Object.keys(queryParameters).length > 0) {
+        const queryParametersElement = queryParameters.map(header => toQueryParameterRow(header.key, header.value));
 
-    if (queryParameters) {
-        queryParametersElement = queryParameters.map(header => toQueryParameterRow(header.key, header.value));
-    }
-
-    return (
-        <div className="card">
-            <header className="card-header has-background-light">
-                <p className="card-header-title">
-                    Query Parameters
-                </p>
-            </header>
-            <div className="card-content">
-                <div className="content">
-                    <table className="table is-bordered">
-                        <tbody>
+        return (
+            <div className="card">
+                <header className="card-header has-background-light">
+                    <p className="card-header-title">
+                        Query Parameters
+                    </p>
+                </header>
+                <div className="card-content">
+                    <div className="content">
+                        <table className="table is-bordered">
+                            <tbody>
                             {queryParametersElement}
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-
-    );
+        );
+    } else {
+        return null;
+    }
 };
 
 export default HttpQueryString;
