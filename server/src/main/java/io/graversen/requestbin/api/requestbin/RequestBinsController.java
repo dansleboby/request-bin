a27df98d629900d81e7c68a38e6405073922b0a0
Martin Graversen <martin@graversen.io>
2021-01-09 12:13:43 +0100

    Revert ":alembic: Asynchronously read raw http request body In an attempt to capture 'application/x-www-form-urlencoded' requests"
    This reverts commit 2cc77ff6

--- server/src/main/java/io/graversen/requestbin/api/requestbin/RequestBinsController.java
+++ server/src/main/java/io/graversen/requestbin/api/requestbin/RequestBinsController.java
@@ -75,7 +75,7 @@ public class RequestBinsController {
             return ResponseEntity.status(HttpStatus.GONE).build();
         }
 
-        serverHttpRequest.getBody().doOnNext(dataBuffer -> {
+        serverHttpRequest.getBody().subscribe(dataBuffer -> {
             final var start = LocalTime.now();
 
             final byte[] requestBodyBytes = new byte[dataBuffer.readableByteCount()];
